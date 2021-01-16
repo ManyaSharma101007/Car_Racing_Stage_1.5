9a3832478989bb05cb2e92e7d297d355fd439d87
Manya Sharma <manyasharma101007@gmail.com>
2021-01-16 13:00:00 +0530

    Car_Racing

--- sketch.js
+++ sketch.js
@@ -8,9 +8,15 @@ var database;
 
 var form, player, game;
 
+var car1;
+var car2;
+var car3;
+var car4;
+
+var cars;
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth,displayHeight);
   database = firebase.database();
   game = new Game();
   game.getState();
